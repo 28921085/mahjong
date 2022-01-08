4787ae9c4fa3cd46d7783d6e2c0f6758ca21d122
tgb3yhn3 <66326426+tgb3yhn3@users.noreply.github.com>
2022-01-08 23:10:45 +0800

    選角頁面優化

--- src/Start.js
+++ src/Start.js
@@ -11,67 +11,44 @@ class Start extends Component{
     choose(character){
       localStorage.setItem('character', character);
     }
-render(props){
-    return  [<div class="container">
-    <div class="row">
-      <div class="col-sm">
+    makeCharacter(img,name,descr,power,no,lock=false){
+      return <div class="col-sm">
       <Card style={{ width: '18rem' }}>
-       
-      
-       <Card.Img variant="top" src={require('./static/character/Jay.png').default} />
+       <Card.Img style={lock?{filter: "blur(" + `8` + "px) brightness(0.7)",height:'200px'}:{height:'200px'}} variant="top" src={
+       require('./static/character/'+img).default
+      } />
        <Card.Body>
-         <Card.Title>阿傑</Card.Title>
+         <Card.Title>{!lock?name:"尚未解鎖"}</Card.Title>
          <Card.Text>
-           Some quick example text to build on the card title and make up the bulk of
-           the card's content.
+           {!lock?descr:"未知"}
          </Card.Text>
        </Card.Body>
        <ListGroup className="list-group-flush">
-         <ListGroupItem>能力一</ListGroupItem>
-         <ListGroupItem>能力二</ListGroupItem>
-         <ListGroupItem>能力三</ListGroupItem>
+         <ListGroupItem>{!lock?power[0]:"等待解鎖"}</ListGroupItem>
+         <ListGroupItem>{!lock?power[1]:"等待解鎖"}</ListGroupItem>
        </ListGroup>
        {/* <Card.Body>
          <Card.Link href="#">Card Link</Card.Link>
          <Card.Link href="#">Another Link</Card.Link>
        </Card.Body> */}
-       <Button onClick={()=>this.choose(0)}>選擇他</Button>
+       <Button disabled={lock} onClick={()=>this.choose(no)}>選擇他</Button>
      </Card>
       </div>
-      <div class="col-sm">
-      <Card style={{ width: '18rem' }}>
-       
+
+    }
+render(props){
+    return  [<div class="container">
+    <div class="row">
+      {this.makeCharacter('Jay.png','阿傑','傑哥不要!!',['知男而上','讓我看看'],0)}
+      {this.makeCharacter('Wang.jpg','王世堅','傑哥不要!!',['Over my dead body','前後為難'],1)}
+      {this.makeCharacter('KP.jpg','柯文哲','傑哥不要!!',['知難而上','前後為難'],2)}
+      {this.makeCharacter('Han.png','韓國瑜','傑哥不要!!',['高雄發大財','烙跑市長'],3,true)}
       
-       <Card.Img variant="top" src={require('./static/character/Wang.jpg').default} />
-       <Card.Body>
-         <Card.Title>王世堅</Card.Title>
-         <Card.Text>
-           Some quick example text to build on the card title and make up the bulk of
-           the card's content.
-         </Card.Text>
-       </Card.Body>
-       
-       <Button onClick={()=>this.choose(1)}>選擇他</Button>
-     </Card>
       </div>
-      <div class="col-sm">
-      <Card style={{ width: '18rem' }}>
-       
+      </div>
       
-       <Card.Img variant="top" src={require('./static/character/KP.jpg').default} />
-       <Card.Body>
-         <Card.Title>柯文哲</Card.Title>
-         <Card.Text>
-           Some quick example text to build on the card title and make up the bulk of
-           the card's content.
-         </Card.Text>
-       </Card.Body>
-       
-       <Button onClick={()=>this.choose(2)}>選擇他</Button>
-     </Card>
-      </div>
-    </div>
-  </div>,
+    
+   
      
      
     ]
