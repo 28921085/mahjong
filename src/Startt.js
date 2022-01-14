7ec25e1976c141b1baf815b9edce4a78d2ca4d67
tgb3yhn3 <66326426+tgb3yhn3@users.noreply.github.com>
2022-01-15 04:30:15 +0800

    角色更新

--- src/Startt.js
+++ src/Startt.js
@@ -6,6 +6,35 @@ class Start extends Component{
      style={
         textAlign:'center'
     }
+    constructor(){
+      super()
+      if(localStorage.getItem("lock")==null){
+          localStorage.setItem("lock",0)
+      }
+      this.lock=localStorage.getItem("lock")
+
+      if(localStorage.getItem("bonusHp")==null){
+        localStorage.setItem("bonusHp",0)
+    }
+    this.bonusHp=localStorage.getItem("bonusHp")
+  
+
+    if(localStorage.getItem("money")==null){
+      localStorage.setItem("money",0)
+  }
+  this.money=localStorage.getItem("money")
+
+    if(localStorage.getItem("bonusAtk")==null){
+      localStorage.setItem("bonusAtk",0)
+  }
+  this.bonusAtk=localStorage.getItem("bonusAtk")
+
+      if(localStorage.getItem("character")==null){
+        localStorage.setItem("character",0)
+    }
+      this.now=localStorage.getItem("character")
+    }
+    
     render(){
       return  <Stack gap={5} className="col-md-5 mx-auto" style={this.style}>
           <h1>麻將<Badge pill bg="success" >
